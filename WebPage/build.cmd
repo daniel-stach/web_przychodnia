67f71eb2b6d7f939f039ff95a1b3a3f4b89efd0b
Dymitr Semeniuk <dima@mixbox.pl>
2016-03-28 22:52:13 +0200

    Fixed build.cmd

--- WebPage/build.cmd
+++ WebPage/build.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-for %%f in (index, lekarze, kontakt, rejestracja,e 404) do (
+for %%f in (index, lekarze, kontakt, rejestracja, 404) do (
 	copy /b src\_header.html + src\_menu.html + src\pages\_%%f.html + src\_footer.html out\%%f.html
 )
